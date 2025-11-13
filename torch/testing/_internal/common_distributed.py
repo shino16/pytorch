cbe7fe98e1ae89f0198d23747ec84e1b2a7da8ec
Wei Wang <weiwan@nvidia.com>
2025-11-13 12:16:17 -0800

    Fix skip logic: do not use system.exit. See   File "/usr/local/lib/python3.12/dist-packages/torch/testing/_internal/common_distributed.py", line 234, in wrapper     sys.exit(test_skip.exit_code) SystemExit: 81
    Resolving
     Conflicts:
    	torch/testing/_internal/common_distributed.py
    due to upstream code addition. No real conflicts

--- torch/testing/_internal/common_distributed.py
+++ torch/testing/_internal/common_distributed.py
@@ -239,9 +239,10 @@ def skip_if_lt_x_gpu(x, *, allow_cpu=False):
                 return func(*args, **kwargs)
             if allow_cpu and not (torch.cuda.is_available() or TEST_HPU or TEST_XPU):
                 return func(*args, **kwargs)
-            test_skip = TEST_SKIPS[f"multi-gpu-{x}"]
-            if not _maybe_handle_skip_if_lt_x_gpu(args, test_skip.message):
-                sys.exit(test_skip.exit_code)
+            #test_skip = TEST_SKIPS[f"multi-gpu-{x}"]
+            #if not _maybe_handle_skip_if_lt_x_gpu(args, test_skip.message):
+            #    sys.exit(test_skip.exit_code)
+            return unittest.skipIf(True, TEST_SKIPS[f"multi-gpu-{x}"].exit_code)(func)
 
         return wrapper
 
